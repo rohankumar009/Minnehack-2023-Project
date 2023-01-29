bab1de391e5d363a6f561b79ddbc1d644e6e590d
ElsdonC <elsdonc21@gmail.com>
2023-01-29 10:51:33 -0600

    connected mongodb atlas

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://127.0.0.1:27017/eventsDB');
+mongoose.connect('mongodb+srv://admin-elsdon:123@cluster0.r5htdck.mongodb.net/eventsDB?appName=mongosh+1.6.1');
 
 const eventSchema = new mongoose.Schema({
     name: String,
